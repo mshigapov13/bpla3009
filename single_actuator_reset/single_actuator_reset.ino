714963a4ef1bf29e105f6a8311458531a7fb54d3
salavat <s.shigapov23@gmail.com>
2024-11-20 23:36:48 +0300

    implemented reset single actuator

--- single_actuator_reset/single_actuator_reset.ino
+++ single_actuator_reset/single_actuator_reset.ino
@@ -1,15 +1,44 @@
+#define ACTUATORS_COUNT 4
+#define ACTUATORS_CONTROL_PINS_COUNT 2
 
-byte in2 = 13;
-byte in1 = 12;
+int COMMON_DELAY = 1500;
+int ACTUATOR_RESET_TIME = 30000;
+
+enum Actuator { BATTERY_SWAPER_ACTUATOR_RIGHT,
+                 BATTERY_SWAPER_ACTUATOR_LEFT };
+
+enum Driver_direction_control_pin {
+  IN1,
+  IN2,
+};
+
+
+int uno_dgt_pins[ACTUATORS_COUNT][ACTUATORS_CONTROL_PINS_COUNT]{
+  { 13, 12 },
+  { 11, 10 },
+  { 7, 6 },
+  { 5, 4 },
+};
+
+void initilization() {
+  for (int i = 0; i < ACTUATORS_COUNT; i++) {
+    for (int j = 0; j < ACTUATORS_CONTROL_PINS_COUNT; j++) {
+      pinMode(uno_dgt_pins[i][j], OUTPUT);
+    }
+  }
+}
 
 void setup() {
-  pinMode(in2, OUTPUT);
-  pinMode(in1, OUTPUT);
+  initilization();
+}
+
+void reset_actuator(Actuator actuatorToReset) {
+  digitalWrite(uno_dgt_pins[actuatorToReset][IN1], LOW);
+  digitalWrite(uno_dgt_pins[actuatorToReset][IN2], HIGH);
+
+  delay(ACTUATOR_RESET_TIME);
 }
 
 void loop() {
-
-  digitalWrite(in1, HIGH);
-  digitalWrite(in2, LOW);
-  delay(6000);
+  reset_actuator(BATTERY_SWAPER_ACTUATOR_RIGHT);
 }
